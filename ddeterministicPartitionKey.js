e7d7cb23e9e8343f31180175fd6b30933f8f8c58
Jefster Farlei Fernandes Caixeta Júnior <jefsterfarlei@gmail.com>
2023-02-20 19:27:59 +0100

    Code refactor

---
+++ ddeterministicPartitionKey.js
@@ -0,0 +1,25 @@
+const crypto = require("crypto");
+
+const TRIVIAL_PARTITION_KEY = "0";
+const MAX_PARTITION_KEY_LENGTH = 256;
+
+exports.deterministicPartitionKey = (event) => {
+  let candidate = TRIVIAL_PARTITION_KEY;
+
+  if (event && event.partitionKey) {
+    candidate = event.partitionKey;
+  } else if (event) {
+    const data = JSON.stringify(event);
+    candidate = crypto.createHash("sha3-512").update(data).digest("hex");
+  }
+
+  if (typeof candidate !== "string") {
+    candidate = JSON.stringify(candidate);
+  }
+
+  if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
+    candidate = crypto.createHash("sha3-512").update(candidate).digest("hex");
+  }
+
+  return candidate;
+};
