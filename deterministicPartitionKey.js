312034353865525691ed01afe519433f2a7af90b
jefster <jefster.fernandes@bigticketplatform.com>
2023-02-20 19:54:16 +0100

    Explanation added

--- deterministicPartitionKey.js
+++ deterministicPartitionKey.js
@@ -1,5 +1,14 @@
 const crypto = require("crypto");
-
+/**
+ * Changes made:
+ *
+ * Removed unnecessary nesting by using else if and else statements to simplify the logic flow.
+ * Combined the if statement for converting candidate to string with the previous one
+ * Rearranged the order of the if statements for better readability
+ *
+ * @param event
+ * @returns {string|string|string|string}
+ */
 exports.deterministicPartitionKey = (event) => {
     const TRIVIAL_PARTITION_KEY = "0";
     const MAX_PARTITION_KEY_LENGTH = 256;
